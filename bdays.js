1a9fa9c83b977f2b6960a839ba3a5005da686288
INV-matt <93540623+INV-matt@users.noreply.github.com>
2022-08-30 21:53:38 +0200

    Create bdays.js

---
+++ bdays.js
@@ -0,0 +1,114 @@
+const birthdays = [
+        {
+            'name': 'Sonia',
+            'date': '04/01'
+        },
+        {
+            'name': 'Filippo',
+            'date': '18/05'
+        },
+        {
+            'name': 'Alessandra',
+            'date': '27/04'
+        },
+        {
+            'name': 'Dorotea',
+            'date': '05/08'
+        },
+        {
+            'name': 'Carlotta',
+            'date': '03/11'
+        },
+        {
+            'name': 'Lorenzo',
+            'date': '16/07'
+        },
+        {
+            'name': 'Jashkarn',
+            'date': '02/03'
+        },
+        {
+            'name': 'Emma',
+            'date': '01/11'
+        },
+        {
+            'name': 'Giulia F.',
+            'date': '30/09'
+        },
+        {
+            'name': 'Giorgia G.',
+            'date': '14/09'
+        },
+        {
+            'name': 'Marco',
+            'date': '08/07'
+        },
+        {
+            'name': 'Francesco',
+            'date': '03/02'
+        },
+        {
+            'name': 'Yousef',
+            'date': '20/07'
+        },
+        {
+            'name': 'Marion',
+            'date': '20/10'
+        },
+        {
+            'name': 'Marta M.',
+            'date': '16/04'
+        },
+        {
+            'name': 'Joe',
+            'date': '14/07'
+        },
+        {
+            'name': 'Tommaso',
+            'date': '21/10'
+        },
+        {
+            'name': 'Margherita',
+            'date': '01/11'
+        },
+        {
+            'name': 'Michele',
+            'date': '10/05'
+        },
+        {
+            'name': 'Paola',
+            'date': '31/01'
+        },
+        {
+            'name': 'Marta R.',
+            'date': '15/05'
+        },
+        {
+            'name': 'Giorgia R.',
+            'date': '29/05'
+        },
+        {
+            'name': 'Valentina',
+            'date': '21/12'
+        },
+        {
+            'name': 'Zoe',
+            'date': '13/11'
+        },
+        {
+            'name': 'Pietro',
+            'date': '08/08'
+        },
+        {
+            'name': 'Jacopo',
+            'date': '15/09'
+        },
+        {
+            'name': 'Giulia T.',
+            'date': '12/09'
+        },
+        {
+            'name': 'Beatrice',
+            'date': '03/04'
+        }
+    ]
